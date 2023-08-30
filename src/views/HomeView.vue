ed7ebb743aa060a11adc5bb6601375cbeebb3aea
Cord Bleibaum <cordbleibaum@gmail.com>
2023-08-31 01:09:22 +0200

    add more content

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,26 +2,40 @@
 </script>
 
 <template>
-    <header>
-        <div class="container">
-            <h1>Cord Bleibaum</h1>
-            <h2>making products from dreams</h2>
+    <div class="container">
+        <div class="flex-divide">
+            <header>
+                <h1>Cord Bleibaum</h1>
+                <h2>making products from dreams</h2>
+                <div class="menu">
+                    <RouterLink to="/">Home</RouterLink>
+                    <RouterLink to="/">Projects</RouterLink>
+                    <RouterLink to="/">Contact me</RouterLink>
+                    <RouterLink to="/legal">Legal</RouterLink>
+                </div>
+            </header>
+            <main>
+                <h2>Hello,</h2>
+                <p>
+                    I'm Cord. Having a broad experience in software development and a passion for entrepreneurship,
+                    I focus on building products from starting from the early stages of ideation to the final product.
+                </p>
+                <p>
+                    I love to work with people who are passionate about their ideas and want to make them a reality.
+                    Continuous learning and improvement are important to me, so I'm always looking for new challenges.
+                </p>
+                <p>
+                    Having a background in Computer Science, I have worked with a large variety of technologies.
+                    These include web development, mobile development, machine learning, computer graphics, FPGA development,
+                    and many more.
+                </p>
+                <p>
+                    I'm always on the lookout for new opportunities. If you have an idea you want to make a reality,
+                    feel free to contact me.
+                </p>
+            </main>
         </div>
-    </header>
-    <main>
-        <div class="container">
-            <p>
-                Having a broad experience in software development and a passion for entrepreneurship,<br>
-                I focus on building products from starting from the early stages of ideation to the final product.
-            </p>
-        </div>
-    </main>
-    <footer>
-        <div class="container">
-            <RouterLink to="/">Home</RouterLink>
-            <RouterLink to="/legal">Legal</RouterLink>
-        </div>
-    </footer>
+    </div>
 </template>
 
 <style scoped>
@@ -32,7 +46,7 @@ h1 {
 }
 
 header {
-    padding-top: 32px;
+    text-align: center;
 }
 
 a {
@@ -40,4 +54,24 @@ a {
     color: #000;
     padding-right: 16px;
 }
+
+.flex-divide {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    height: 100vh;
+    align-items: center;
+    gap: 64px;
+}
+
+.flex-divide > * {
+    flex: 1;
+}
+
+.menu {
+    border-top: 2px solid #000;
+    padding-top: 16px;
+}
+
+
 </style>
