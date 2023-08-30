94de1e6c4d38b652ccc74230d356e0378291612e
Cord Bleibaum <cordbleibaum@gmail.com>
2023-08-31 00:37:29 +0200

    some starting text

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,32 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <RouterView />
+    <RouterView />
 </template>
 
-<style scoped>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Lato&family=Montserrat:wght@300&family=Nunito+Sans:wght@500&display=swap');
 
+*, *::before, *::after {
+    box-sizing: border-box;
+}
+
+.container {
+    max-width: 1440px;
+    margin: 0 auto;
+    padding: 0 1rem;
+}
+
+html {
+    font-family: sans-serif;
+}
+
+body {
+    margin: 0;
+    padding: 0;
+}
+
+h1 {
+    font-family: 'Montserrat', sans-serif;
+}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,42 @@
 </script>
 
 <template>
-  <main>
-    
-  </main>
+    <header>
+        <div class="container">
+            <h1>Cord Bleibaum</h1>
+            <h2>making products from dreams</h2>
+        </div>
+    </header>
+    <main>
+        <div class="container">
+            <p>
+                Having a broad experience in software development and a passion for entrepreneurship,<br>
+                I focus on building products from starting from the early stages of ideation to the final product.
+            </p>
+        </div>
+    </main>
+    <footer>
+        <div class="container">
+            <RouterLink to="/">Home</RouterLink>
+            <RouterLink to="/legal">Legal</RouterLink>
+        </div>
+    </footer>
 </template>
+
+<style scoped>
+h1 {
+    font-size: 64px;
+    margin: 0;
+    padding: 0;
+}
+
+header {
+    padding-top: 32px;
+}
+
+a {
+    text-decoration: none;
+    color: #000;
+    padding-right: 16px;
+}
+</style>
