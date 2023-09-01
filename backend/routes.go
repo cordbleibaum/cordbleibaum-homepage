6db581f6a0aaadd1fb1b7d55c491b42c5485d78b
Cord Bleibaum <cordbleibaum@gmail.com>
2023-09-01 19:22:03 +0200

    add basic backend server code in go

---
+++ backend/routes.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"regexp"
+	"strings"
+)
+
+var routes = []route{}
+
+func newRoute(method, pattern string, handler http.HandlerFunc) route {
+	return route{method, regexp.MustCompile("^" + pattern + "$"), handler}
+}
+
+type route struct {
+	method  string
+	regex   *regexp.Regexp
+	handler http.HandlerFunc
+}
+
+type ctxKey struct{}
+
+func getField(r *http.Request, index int) string {
+	fields := r.Context().Value(ctxKey{}).([]string)
+	return fields[index]
+}
+
+func serve(w http.ResponseWriter, r *http.Request) {
+	var allow []string
+	for _, route := range routes {
+		matches := route.regex.FindStringSubmatch(r.URL.Path)
+		if len(matches) > 0 {
+			if r.Method != route.method {
+				allow = append(allow, route.method)
+				continue
+			}
+			log.Println("Handling", r.Method, "request to", r.URL.Path)
+			w.Header().Set("Content-Type", "application/json")
+			ctx := context.WithValue(r.Context(), ctxKey{}, matches[1:])
+			route.handler(w, r.WithContext(ctx))
+			return
+		}
+	}
+	if len(allow) > 0 {
+		w.Header().Set("Allow", strings.Join(allow, ", "))
+		http.Error(w, "405 method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	http.NotFound(w, r)
+}
---
+++ backend/server.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/rs/cors"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var db *mongo.Database
+var ctx context.Context
+
+func main() {
+	ctx = context.Background()
+
+	log.Println("Starting server...")
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("You must set your 'PORT' environment variable.")
+	}
+
+	mongodb_uri := os.Getenv("MONGODB_URI")
+	if mongodb_uri == "" {
+		log.Fatal("You must set your 'MONGODB_URI' environmental variable. See\n\t https://www.mongodb.com/docs/drivers/go/current/usage-examples/#environment-variable")
+	}
+
+	log.Println("Connecting to MongoDB using URI: " + mongodb_uri)
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongodb_uri))
+	if err != nil {
+		panic(err)
+	}
+
+	mongodb_dbname := os.Getenv("MONGODB_DBNAME")
+	if mongodb_dbname == "" {
+		log.Fatal("You must set your 'MONGODB_DBNAME' environmental variable.")
+	}
+	db = client.Database(mongodb_dbname)
+
+	server := &http.Server{
+		Addr: ":" + port,
+	}
+	cors_config := cors.New(cors.Options{
+		AllowedOrigins: []string{
+			"https://cordbleibaum.com",
+			"https://www.cordbleibaum.com",
+			"http://localhost:3000",
+			"https://cordbleibaum.de",
+			"https://www.cordbleibaum.de",
+		},
+		AllowCredentials: true,
+		Debug:            false,
+	})
+
+	handler := cors_config.Handler(http.HandlerFunc(serve))
+	server.Handler = handler
+
+	go func() {
+		if err := server.ListenAndServe(); err != http.ErrServerClosed {
+			log.Fatal(err)
+		}
+
+		log.Println("Disconnecting from MongoDB")
+		if err := client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+
+	<-stop
+
+	log.Println("Shutting down server")
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatal(err)
+	}
+}
