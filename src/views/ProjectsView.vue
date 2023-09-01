95cfef924df654a7cb466c3587fded3ab670126e
Cord Bleibaum <cordbleibaum@gmail.com>
2023-09-01 15:43:16 +0200

    add a project to list

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,5 +1,24 @@
 <template>
     <main>
-        
+        <h2>Projects</h2>
+        <p>
+            In the last years I completed many projects successfully, and many more are coming  Here are some of them:
+        </p>
+        <ul>
+            <li>
+                Online-Casino-Verluste.de : Developed a website and a backend for a client to help people who lost money in online casinos to get their money back.
+                The main goal of the website is to further automate the process. It integrates with DocuSign and Email services to automatically send out documents, and requests afterwards.
+                Furthermore there is an admin panel to manage the cases and the users, supporting the client in his daily work.
+            </li>
+            <li>
+                
+            </li>
+        </ul>
     </main>
 </template>
+
+<style scoped>
+p, li {
+    font-family: 'Open Sans', sans-serif;
+}
+</style>
