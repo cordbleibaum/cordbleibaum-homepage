22d8dca6f60b1e945d5e3971e0785f0ee20f8dfd
Cord Bleibaum <cordbleibaum@gmail.com>
2023-09-03 12:39:14 +0200

    contact request

--- frontend/src/views/ContactView.vue
+++ frontend/src/views/ContactView.vue
@@ -1,3 +1,35 @@
+<script setup>
+import { ref } from 'vue'
+
+const name = ref('')
+const email = ref('')
+const message = ref('')
+
+async function submitForm() {
+    const response = await fetch('https://api.cordbleibaum.de/contact', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            name: name.value,
+            email: email.value,
+            message: message.value
+        })
+    })
+
+    name = ''
+    email = ''
+    message = ''
+
+    if (response.ok) {
+        alert('Your message has been sent!')
+    } else {
+        alert('An error occurred while sending your message.')
+    }
+}
+</script>
+
 <template>
     <main>
         <h2>Contact me</h2>
@@ -11,17 +43,17 @@
         <form>
             <div>
                 <label for="name">Name</label>
-                <input type="text" id="name" name="name" placeholder="Your name" required>
+                <input type="text" id="name" name="name" placeholder="Your name" required v-model="name">
             </div>
             <div>
                 <label for="email">Email</label>
-                <input type="email" id="email" name="email" placeholder="Your email" required>
+                <input type="email" id="email" name="email" placeholder="Your email" required v-model="email">
             </div>
             <div>
                 <label for="message">Message</label>
-                <textarea id="message" name="message" placeholder="Your message" required></textarea>
+                <textarea id="message" name="message" placeholder="Your message" required v-model="message"></textarea>
             </div>
-            <button type="submit">Send</button>
+            <button @click="submit">Send</button>
         </form>
     </main>
 </template>
